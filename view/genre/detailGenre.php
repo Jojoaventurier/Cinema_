7e08f53ac927b485ee25376d201d616c7cd5c425
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-06-07 09:58:42 +0200

    updt

--- view/genre/detailGenre.php
+++ view/genre/detailGenre.php
@@ -1,8 +1,8 @@
 <?php ob_start(); ?>
 
-<h2><?= $genre = $requeteGenre->fetch(); echo $genre["libelle"]; ?></h2>
+<h2><?= $libelleGenre = $requeteGenre->fetch(); echo $libelleGenre["libelle"]; ?></h2>
 
-<p> Il y a <?= $requete->rowCount() ?> films du genre <?= $genre = $requeteGenre->fetch(); echo $genre["libelle"]; ?></p>
+<p> Il y a <?= $requete->rowCount() ?> films du genre <? echo $libelleGenre["libelle"]; ?></p>
 
 <table>
     <thead>
@@ -13,17 +13,17 @@
     </thead>
     <tbody>
         <?php
-            foreach($requete->fetchAll() as $genre) { ?>
+            foreach($requete->fetchAll() as $film) { ?>
                 <tr>
-                    <td><?= $genre["titre"] ?></td>
-                    <td><?= $genre["anneeSortieFrance"] ?></td>
+                    <td><?= $film["titre"] ?></td>
+                    <td><?= $film["anneeSortieFrance"] ?></td>
                 </tr>
         <?php } ?>
     </tbody>
 </table>
 
 <?php
-$titre = "Liste des films pour le genre " . $genre["libelle"];
-$titre_secondaire = "Liste des films pour le genre" . $genre["libelle"];
+$titre = "Liste des films pour le genre " . $libelleGenre["libelle"];
+$titre_secondaire = "Liste des films pour le genre" . $libelleGenre["libelle"];
 $contenu = ob_get_clean();
 require "view/template.php";
