887719bf0cc7bbb2369acb5e9d69587c717f0e27
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-06-07 16:48:17 +0200

    updt

--- view/accueil.php
+++ view/accueil.php
@@ -1,11 +1,14 @@
-<?php ob_start();
+<?php ob_start(); 
 
-$contenu = ob_get_clean();
-require "template.php";
+
+
+
+
 
 $titre = "Accueil";
 $titre_secondaire = "Accueil";
-
+$contenu = ob_get_clean();
+require "template.php";
 ?>
 
 
--- view/template.php
+++ view/template.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/style.css" class="style">
-    <title><?= $titre ?></title>
+    <title><?php echo $titre ?></title>
 </head>
 <body>
     <nav>
