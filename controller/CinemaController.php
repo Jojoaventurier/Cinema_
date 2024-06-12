5ec73f1e01ffd98953f9e8da9d5c121274156cad
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-06-12 09:11:28 +0200

    finished th e dispatch

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -15,22 +15,6 @@ class CinemaController {
         require "view/accueil.php";
     }
 
-    /**
-     * Lister les films
-     */
-    public function listeFilms() {
-
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->query("
-            SELECT f.id_film, titre, YEAR(anneeSortieFrance) as 'year', CONCAT(prenom, ' ', nom) as 'realisateur', f.id_realisateur
-            FROM film f, realisateur re, personne p
-            WHERE f.id_realisateur = re.id_realisateur
-            AND re.id_personne = p.id_personne
-        ");
-
-        require "view/listeFilms.php";
-    }
-
 
     /**
      * Lister les genres
@@ -53,42 +37,6 @@ class CinemaController {
 
 
     //========================================DETAIL=====================================//
-
-    /**
-     * Détails d'un film
-     */
-    public function detailFilm($id) {
-
-        $pdo = Connect::seConnecter();
-
-        $requeteTitre = $pdo->prepare("
-            SELECT titre
-            FROM film
-            WHERE id_film= :id");
-        $requeteTitre->execute(["id" => $id]);
-
-        $requete = $pdo->prepare("
-            SELECT f.id_film, titre, anneeSortieFrance as 'sortie', duree, prenom, nom, re.id_realisateur, resume 
-            FROM film f, realisateur re, personne p
-            WHERE f.id_realisateur = re.id_realisateur
-            AND re.id_personne = p.id_personne 
-            AND id_film = :id
-        ");
-        $requete->execute(["id" => $id]);
-
-        $requeteCasting = $pdo->prepare("
-            SELECT f.id_film, c.id_acteur, prenom, nom, nomRole
-            FROM personne p, film f, casting c, acteur a, role r
-            WHERE p.id_personne = a.id_personne
-            AND f.id_film = c.id_film
-            AND c.id_acteur = a.id_acteur
-            AND c.id_role = r.id_role
-            AND f.id_film = :id
-        ");
-        $requeteCasting->execute(["id" => $id]);
-
-        require "view/film/detailFilm.php";
-    }
 
 
     /**
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use Controller\CinemaController;
 use Controller\PersonneController;
+use Controller\FilmController;
 
 spl_autoload_register(function ($class_name) {
     include  $class_name . '.php';
@@ -9,6 +10,7 @@ spl_autoload_register(function ($class_name) {
 
 $ctrlCinema = new CinemaController();
 $ctrlPersonne = new PersonneController();
+$ctrlFilm = new FilmController();
 
 
 $id = (isset($_GET["id"])) ? $_GET["id"] : null;
@@ -18,11 +20,11 @@ if (isset($_GET["action"])) {
     switch($_GET["action"]) {
         // 
         case "accueil" : $ctrlCinema->pageAccueil(); break;
-        case "listeFilms" : $ctrlCinema->listeFilms(); break;
+        case "listeFilms" : $ctrlFilm->listeFilms(); break;
         case "listeActeurs" : $ctrlPersonne->listeActeurs(); break;
         case "listeRealisateurs" : $ctrlPersonne->listeRealisateurs(); break;
         case "listeGenres" : $ctrlCinema->listeGenres(); break;
-        case "detailFilm" : $ctrlCinema->detailFilm($id); break;
+        case "detailFilm" : $ctrlFilm->detailFilm($id); break;
         case "detailActeur" : $ctrlPersonne->detailActeur($id); break;
         case "detailRealisateur" : $ctrlPersonne->detailRealisateur($id); break;
         case "detailGenre" : $ctrlCinema->detailGenre($id); break;
