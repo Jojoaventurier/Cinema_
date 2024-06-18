8efae24099a8701de7ad6e96e47ec55fc888b6f9
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-06-18 15:32:14 +0200

    added comments

--- view/film/detailFilm.php
+++ view/film/detailFilm.php
@@ -2,19 +2,19 @@
 $titreFilm = $requeteTitre->fetch() ?>
 
 
-
+<!-- lien vers la page formulaire de modification des informations d'un film -->
 <a class='link bouton' href="index.php?action=modificationFilm&id=<?=$titreFilm['id_film']?>">MODIFIER LE FILM</a>
                 
 <?php
     foreach($requete->fetchAll() as $film) { ?>
         
-            <p>Sortie le : <?= $film["sortie"] ?></p>
-            <p>Réalisé par <a class='link' href="index.php?action=detailRealisateur&id=<?= $film['id_realisateur']?>"><?= $film["prenom"] . " " . $film["nom"] ?></a></p>
-            <p>Durée : <?= $film["durée"] ?></p>
+            <p>Sortie le : <?= $film["sortie"] ?></p><!-- récupère la date de sortie du film -->
+            <p>Réalisé par <a class='link' href="index.php?action=detailRealisateur&id=<?= $film['id_realisateur']?>"><?= $film["prenom"] . " " . $film["nom"] ?></a></p><!-- génère un lien vers la page détail du réalisateur -->
+            <p>Durée : <?= $film["durée"] ?></p><!-- récupère la durée du film -->
 <?php } ?>
 
 <p>
-    <?php 
+    <?php // affiche tous les genres associés au film
         foreach($requeteGenres->fetchAll() as $genre) { ?>
 
         <a class="link" href="index.php?action=detailGenre&id=<?= $genre['id_genre']?>"><?=$genre['libelle']?></a>
@@ -23,18 +23,18 @@ $titreFilm = $requeteTitre->fetch() ?>
     ?>
 </p>
 
-<?php
+<?php // récupère les acteurs et leur rôle correspondant pour le film
 foreach($requeteCasting->fetchAll() as $casting) { ?>
     
     <table>
-        <tr>
+        <tr><!-- génère un lien vers la page détail de chaque acteur -->
             <td><a class='link' href="index.php?action=detailActeur&id=<?=$casting['id_acteur']?>"><?= $casting["prenom"]. " ". $casting["nom"] ?></a></td>
-            <td><?= " : ". $casting["nomRole"] ?></td>
+            <td><?= " : ". $casting["nomRole"] ?></td> <!-- récupère le nom du rôle associé à l'acteur pour le film -->
         </tr>
 <?php } ?>
     </table>
 
-    <p id="synopsis"><?= $film["synopsis"] ?>
+    <p id="synopsis"><?= $film["synopsis"] ?> <!-- affiche le synopsis du film enregistré en BDD -->
 
 <?php
 $titre = $titreFilm["titre"];
--- view/genre/detailGenre.php
+++ view/genre/detailGenre.php
@@ -2,6 +2,7 @@
  $libelleGenre = $requeteGenre->fetch(); 
 ?>
 
+<!-- lien vers la page formulaire de modification d'un genre (édition du libellé) -->
 <a class='link bouton' href="index.php?action=afficherModifierGenre&id=<?=$libelleGenre['id_genre']?>">MODIFIER LE GENRE</a>
 
 <h2><?= $libelleGenre["libelle"]; ?></h2>
@@ -10,12 +11,12 @@
 <table>
     
     <tbody>
-        <?php
+        <?php // récupère la liste de tous les films associés au genre
             foreach($requete->fetchAll() as $film) { ?>
                 <tr>
-                    <td><a class='link' href="index.php?action=detailFilm&id=<?=$film['id_film']?>"><?= $film["titre"] ?></td>
-                    <td>(<?= $film["sortie"] ?>)</td>
-                    <td><a class='link' href="index.php?action=detailRealisateur&id=<?= $film['id_realisateur']?>"><?= $film["realisateur"] ?></td>
+                    <td><a class='link' href="index.php?action=detailFilm&id=<?=$film['id_film']?>"><?= $film["titre"] ?></td><!-- génère un lien vers la page détail du film -->
+                    <td>(<?= $film["sortie"] ?>)</td> <!-- affiche la date de sortie du film -->
+                    <td><a class='link' href="index.php?action=detailRealisateur&id=<?= $film['id_realisateur']?>"><?= $film["realisateur"] ?></td> <!-- génère un lien qui renvoie vers la page détail du réalisateur -->
                 </tr>
         <?php } ?>
     </tbody>
