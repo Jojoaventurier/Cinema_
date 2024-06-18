53bffbad47892014ac6a61ceb3e1334bb864beea
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-06-18 15:44:18 +0200

    updt

--- model/Connect.php
+++ model/Connect.php
@@ -1,21 +1,33 @@
 <?php
-
+ 
 namespace Model;
-
-abstract class Connect {
-
-    const HOST = "localhost";
-    const DB = "cinema";
-    const USER = "root";
-    const PASS = "";
-
-    public static function seConnecter() {
+ 
+// Définition d'une classe abstraite Connect dans l'espace de noms Model
+abstract class Connect
+{
+ 
+    // Définition des constantes pour les paramètres de connexion à la base de données
+    const HOST = "localhost"; // Nom d'hôte de la base de données
+    const DB = "cinema"; // Nom de la base de données
+    const USER = "root"; // Nom d'utilisateur de la base de données
+    const PASS = ""; // Mot de passe de la base de données (vide dans ce cas)
+ 
+    // Méthode statique pour établir une connexion à la base de données
+    public static function seConnecter()
+    {
         try {
+            // Création et retour d'un objet PDO pour la connexion à la base de données
             return new \PDO(
-                "mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8", self::USER, self::PASS);
-        } catch(\PDOException $ex) {
+                "mysql:host=" . self::HOST . ";dbname=" . self::DB . ";charset=utf8", // Chaîne de connexion PDO
+                self::USER, // Nom d'utilisateur de la base de données
+                self::PASS // Mot de passe de la base de données
+            );
+        } catch (\PDOException $ex) {
+            // En cas d'erreur lors de la connexion, retourne le message d'erreur
             return $ex->getMessage();
         }
     }
 }
+?>
+
 
--- view/acteur/detailActeur.php
+++ view/acteur/detailActeur.php
@@ -2,17 +2,18 @@
 
 $acteur = $requete->fetch() ?>
 
+<!-- lien vers la page de modification d'un acteur -->
 <a class='link bouton' href="index.php?action=afficherModifierActeur&id=<?= $acteur['id_acteur'] ?>">MODIFIER LES INFORMATIONS</a>
 
-<p>Né(e) le <?= $acteur['dateNaissance'] ?>
+<p>Né(e) le <?= $acteur['dateNaissance'] ?> <!-- récupère la date de naissance de l'acteur -->
 <table>
     <tbody>
-        <?php
+        <?php // récupère et affiche tous les rôles et le nom des films dans lesquels l'acteur a joué
             foreach($requeteRoles->fetchAll() as $film) { ?>
                 <tr>
-                    <td><a class='link' href="index.php?action=detailFilm&id=<?=$film['id_film']?>"><?= $film["titre"] ?></td>
-                    <td>(<?= $film["sortie"] ?>)</td>
-                    <td><?= $film["nomRole"] ?></td>
+                    <td><a class='link' href="index.php?action=detailFilm&id=<?=$film['id_film']?>"><?= $film["titre"] ?></td> <!-- génère un lien vers le détail du film -->
+                    <td>(<?= $film["sortie"] ?>)</td> <!-- récupère et affiche la date de sortie du film -->
+                    <td><?= $film["nomRole"] ?></td> <!-- récupère et affiche le nom du rôle correspondant pour le film -->
                 </tr>
         <?php } ?>
     </tbody>
